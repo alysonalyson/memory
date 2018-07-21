6e86289b493557a2af96a5e10eb650c3f59ccaac
Alyson Turley <alyson.turley@gmail.com>
2018-07-20 19:55:33 -0500

    Added matching functionality and win pop-up

--- js/main.js
+++ js/main.js
@@ -1,32 +1,48 @@
 $(document).ready(() => {
   let moveCounter = 0;
 
-  $('.front').on('click', event => {
-    $(event.currentTarget).children('i').toggleClass('hidden');
-    $(event.currentTarget).addClass('active');
-    moveCounter++;
-    $('#move-counter').html(moveCounter);
-
-    window.setTimeout(function () {
-      if (moveCounter %2 === 0) {
-        $('.active').children('i').toggleClass('hidden');
-        $('.active').removeClass('active').addClass('front');
-      }
-    }, 1800);
-  });
-
-  $('.back').on('click', event => {
-    $(event.currentTarget).toggleClass('front');
-  });
-
   let tiles = $('.tile');
   for(let i = 0; i < tiles.length; i++) {
     let index1 = Math.floor(Math.random() * tiles.length - 1) + 1;
     let index2 = Math.floor(Math.random() * tiles.length - 1) + 1;
     tiles.eq(index1).before(tiles.eq(index2));
-  }
+  };
 
   $('#refresh').on('click', () => {
     window.location.reload(true);
   });
+
+  $('#reset').on('click', () => {
+    window.location.reload(true);
+  });
+
+  $('.tile').on('click', event => {
+    $(event.currentTarget).children('i').show();
+    $(event.currentTarget).addClass('active');
+    moveCounter++;
+    $('.move-counter').html(moveCounter);
+
+    let target1 = $('.active')[0];
+    let target2 = $('.active')[1];
+    if (target1.isEqualNode(target2)){
+      $('.active').effect('highlight');
+      $('.active').addClass('matched').removeClass('active');
+    } else {
+      if (moveCounter > 0 && moveCounter %2 === 0) {
+        $('.active').effect('bounce', 'slow');
+        $('.tile').addClass('no-click');
+        window.setTimeout(function () {
+          $('.active').children('i').hide();
+          $('.active').removeClass('active').addClass('front');
+          $('.tile').removeClass('no-click');
+        }, 800);
+      };
+    }
+    let pairs = $('.matched');
+    if(pairs.length == 16) {
+      $('#win-pop-up').slideDown();
+    }
+    console.log(pairs.length);
+  });
+
 });
