638707bc57ae74214e141320c10d40d051c4adb7
Alyson Turley <alyson.turley@gmail.com>
2018-07-25 07:49:19 -0500

    improved JS comments and CSS formatting

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
+  //initiate move tracker
   let moveCounter = 0;
-
   //timer function
   let countUp = new Date(); localStorage.setItem('startDate', countUp);
   let timer = setInterval(function() {
@@ -17,27 +17,29 @@ $(document).ready(() => {
     $('.timer').html(minutes + ':' + seconds);
 }, 1000);
 
+//randomize tile placement
   let tiles = $('.tile');
   for(let i = 0; i < tiles.length; i++) {
     let index1 = Math.floor(Math.random() * tiles.length - 1) + 1;
     let index2 = Math.floor(Math.random() * tiles.length - 1) + 1;
     tiles.eq(index1).before(tiles.eq(index2));
   };
-
+//refresh & reset buttons
   $('#refresh').on('click', () => {
     window.location.reload(true);
   });
-
   $('#reset').on('click', () => {
     window.location.reload(true);
   });
 
+//flip tiles
   $('.tile').on('click', event => {
     $(event.currentTarget).children('i').show();
     $(event.currentTarget).addClass('active');
+    //count moves
     moveCounter++;
     $('.move-counter').html(moveCounter);
-
+   // keep track of moves to change star rating
     if (moveCounter === 30) {
       $('.s4').hide();
     } else if (moveCounter === 40) {
@@ -48,12 +50,10 @@ $(document).ready(() => {
       $('.s3').hide();
       $('.s2').hide();
     }
-
+    //check for matches
     let pair = $('.active');
     let target1 = pair[0];
     let target2 = pair[1];
-
-    console.log(pair);
 
     if (target1.isEqualNode(target2)){
       console.log(target1);
@@ -72,7 +72,7 @@ $(document).ready(() => {
           $('.active').removeClass('active').addClass('front');
         }, 400);
     }
-
+    //end the game when all tiles are matched
     let pairs = $('.matched');
     if(pairs.length == 16) {
       clearInterval(timer);
